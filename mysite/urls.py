f970dba9552ac9db10956d8585cb6444d338eb18
tony-blackie <jim.casey321@gmail.com>
2017-06-05 12:25:56 +0300

    Add new view with time display

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from mysite.views import hello, my_homepage_view
+from mysite.views import hello, current_datetime
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
-    url(r'^$', my_homepage_view),
+    url(r'^time/$', current_datetime),
 ]
